1579ccd623057c9aba8009a70705bdb21d20bfb9
Fabio Toscano <fabio.toscano@gmail.com>
2024-03-28 15:50:28 -0300

    deploy validação testes IP

--- server/main.go
+++ server/main.go
@@ -101,14 +101,14 @@ func (s *dadosReceitaServer) ProcessImport(ctx context.Context, req *dadosReceit
 }
 
 func stepsImport() {
-	/*service.ProcessCSVCnae()
+	service.ProcessCSVCnae()
 	service.ProcessCSVMotivoSituacaoCadastral()
 	service.ProcessCSVMunicipio()
 	service.ProcessCSVNaturezaJuridica()
 	service.ProcessCSVPais()
 	service.ProcessCSVQualificacaoSocio()
 	service.ProcessCSVEmpresa()
-	service.ProcessCSVSocio()*/
-	//service.ProcessCSVEstabelecimento()
-	//service.ProcessCSVOptanteSimples()
+	service.ProcessCSVSocio()
+	service.ProcessCSVEstabelecimento()
+	service.ProcessCSVOptanteSimples()
 }
